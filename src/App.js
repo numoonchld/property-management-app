977b210787aaed96ad0792da6a8cbfc1a5144b3e
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2021-08-10 22:02:49 +0530

    login functionality built

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-
+import Logout from './components/Logout'
+import { AuthContext } from './contexts/AuthContext';
 
 function App() {
-  return <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={Login} />
-      <Route exact path='/dashboard' component={Dashboard} />
-    </Switch>
-  </BrowserRouter>
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [users, setUsers] = useState([{ username: `superuser0`, password: `0000`, isActive: true, level: 0 }])
+  const [loggedInUser, setLoggedInUser] = useState(null)
+
+
+  return <div className="App">
+    <AuthContext.Provider value={{ users, isAuthenticated, loggedInUser, setUsers, setIsAuthenticated, setLoggedInUser }}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/logout' component={Logout} />
+        </Switch>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  </div>
+
+
 
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,45 @@
-import React, { Fragment } from 'react'
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+import { withRouter } from 'react-router-dom'
 
-function Login() {
+function Login({history}) {
+
+    const { users, setIsAuthenticated, setLoggedInUser} = useContext(AuthContext)
+
+    const [username, setUsername] = useState(``)
+    const [password, setPassword] = useState(``)    
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const loginMatch = users.filter( user => user.username === username && user.password === password && user.isActive)
+
+        if (loginMatch.length === 1) {
+            setLoggedInUser(loginMatch[0])
+            setIsAuthenticated(true)
+            history.push('/dashboard')
+        }
+        else {
+            window.alert('Login Credentials incorrect or Inactive User!')
+        }
+
+        
+        
+
+    }
     return <>
 
         <div className='card w-75 mx-auto my-5 p-5'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Username</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={username} onChange={(event) => {setUsername(event.target.value)}}/>
                     
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
@@ -22,4 +49,4 @@ function Login() {
 
 }
 
-export default Login
+export default withRouter(Login)
