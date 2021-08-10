0646a726635b99ae4dfcb3dfcb70dbe5600de239
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2021-08-11 00:53:35 +0530

    owner dashboard works

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -13,10 +13,10 @@ function Logout({ history }) {
     }
 
     return <>
-        <div className='card mx-auto w-75 p-5 my-5'>
-            Are you sure you want to logout?
-            <button className='btn btn-success' onClick={logout}> Yes </button>
-            <button className='btn btn-primary' onClick={history.goBack()}> No </button>
+        <div className='card mx-auto w-75 p-5 my-5 text-center'>
+            <h4>Are you sure you want to logout?</h4>
+            <button className='btn btn-success my-3' onClick={logout}> Yes </button>
+            <button className='btn btn-primary my-3' onClick={() => history.goBack()}> No </button>
         </div>
     </>
 }
--- src/components/OwnerPropertyTable.js
+++ src/components/OwnerPropertyTable.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PropertyContext } from '../contexts/PropertyContext'
+import { AuthContext } from '../contexts/AuthContext'
 
-const TableRow = () => {
-    return <></>
+const TableRow = ({ property }) => {
+
+    const { properties, setProperties } = useContext(PropertyContext)
+
+
+    const handlePropertyApproval = (event) => {
+        
+        setProperties(properties.map( eachProperty => {
+            if (eachProperty.propertyName === property.propertyName) {
+                return {
+                    ...eachProperty,
+                    isApproved: event.target.checked
+                }
+            }
+            return eachProperty
+        }))
+    }
+
+    return <>
+        <tr>
+            <td>
+                {property.propertyName}
+            </td>
+            <td>
+                <input
+                    type='checkbox'
+                    checked={property.isApproved}
+                    onChange={handlePropertyApproval}
+                />
+            </td>
+            <td>
+                {property.isSold ? property.propertyBuyer : `Not Sold`}
+            </td>
+            <td>
+                {property.price}
+            </td>
+        </tr></>
 }
 
 function OwnerPropertyTable() {
-    return <></>
+
+    const { properties } = useContext(PropertyContext)
+    const { loggedInUser } = useContext(AuthContext)
+
+    return <>
+        <table className="table mt-3 mb-5 border ">
+            <thead>
+                <th>Property Name</th>
+                <th>Approved</th>
+                <th>Buyer</th>
+                <th>Price</th>
+            </thead>
+            <tbody>
+                {properties.filter(property => property.propertyOwner === loggedInUser.username ).filter(property => property.isActive === true).map(property => <TableRow property={property} />)}
+            </tbody>
+        </table>
+    </>
 }
 
 export default OwnerPropertyTable
